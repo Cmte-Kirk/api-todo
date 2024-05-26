aa2c280174dd87791344a555ea19640b3de87682
Cmte-Kirk <kirk.java@gmail.com>
2024-05-26 16:05:44 -0300

    implementado configurações de deploy no vercel

--- api_todo/settings.py
+++ api_todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m9k2ii*riq&b&cv#(h^&rgjvizd4xk@k)i(_)(+m8vv+qu&y9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -125,3 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
