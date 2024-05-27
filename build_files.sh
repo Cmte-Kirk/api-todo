2e3450508610348947ac6c58a549fbacdba96a07
Cmte-Kirk <kirk.java@gmail.com>
2024-05-27 17:40:43 -0300

    correção de configuração para rodar no postgress do vercel 4

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,5 @@
 echo " BUILD START"
 
-var1="backports.zoneinfo==0.2.1"
-var2='backports.zoneinfo==0.2.1;python_version<"3.9"'
-sed -i -e "s/$var1/$var2/g" requirements.txt
-
 python3.9 -m pip install -r requirements.txt
 
 echo " MAKE MIGRATIONS..."
